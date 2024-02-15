f0e8d88cb9ae2795b9502fef146b123448e36ee2
priyanshu kumar <priyanhuk2003@gmail.com>
2024-02-15 12:50:23 +0530

    new changes

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-
+const clickableDivs = document.querySelectorAll(".tiles"); 
+var tokens=["1","2","3","4","5","6","7","8","9"];
 var counter=0;
 
-var tokens=["1","2","3","4","5","6","7","8","9"];
 
-
-const clickableDivs = document.querySelectorAll(".cell"); 
-
+function ToCheck(ValToCheck,Array){
+    if (Array.includes(ValToCheck)) {
+        return true;
+      } else {
+        return false;
+      }
+}
+function getElementId(element) {
+    return element.id || element.getAttribute('id');
+  }
+function removeElement(valueToRemove, array) {
+    const newArray = array.filter(element => element !== valueToRemove);
+    if (array.length > 0) {
+      array.splice(0, array.length, ...newArray); 
+    }
+    return newArray;
+  }
 
 function move(cell){
-    if(counter==0){
-        cell.textContent = "O";
-        cell.style.background="red";
-        counter=1;
+    if(ToCheck(getElementId(cell),tokens)){
+        if(counter==0){
+            cell.textContent = "O";
+            cell.style.background="red";
+            counter=1;
+        }
+        else{
+            cell.textContent = "X";
+            cell.style.background="green";
+            counter=0;
+        }
+        tokens=removeElement(getElementId(cell),tokens)
     }
     else{
-        cell.textContent = "X";
-        cell.style.background="green";
-        counter=0;
+        console.log("Invalid")
     }
-   console.log(this.id)
+    
+
 }
